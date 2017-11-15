08fe1cfb7698fdfbaf34a090dc1204758d530317
David Röttger <davidrottger@tests-MacBook-Pro.local>
2017-11-15 11:35:12 +0100

    outer div innner div added. mouse events to trigger add classes

--- scripts/index.js
+++ scripts/index.js
@@ -1,58 +1,34 @@
-var gedicht = {
-	titel:"Der Panther",
-	strophen: ["Sein Blick ist vom Vorübergehn der Stäbe so müd geworden, dass er nichts mehr hält. Ihm ist, als ob es tausend Stäbe gäbe und hinter tausend Stäben keine Welt.", "Der weiche Gang geschmeidig starker Schritte, der sich im allerkleinsten Kreise dreht, ist wie ein Tanz von Kraft um eine Mitte, in der betäubt ein großer Wille steht.", "Nur manchmal schiebt der Vorhang der Pupille sich lautlos auf -. Dann geht ein Bild hinein, geht durch der Glieder angespannte Stille - und hört im Herzen auf zu sein." ] 
- }
+
+var container = document.querySelector(".container");
+
+var create_div_outer = document.createElement("DIV");
+
+var create_div_inner = document.createElement("DIV");
+
+var outerlayer = container.appendChild(create_div_outer)
+
+outerlayer.classList.add("outerlayer");
+
+var innerlayer = document.querySelector(".outerlayer").appendChild(create_div_inner);
+
+innerlayer.classList.add("innerlayer");
 
 
-// global selector
-var container = document.querySelector(".gedicht");
+//mouseover add class move to innerlayer to animate the x position
 
-var panther = document.querySelector(".panther");
-
-// create paragraph for verse and attach class to every paragraph
-
-for(var i = 0; i < gedicht.strophen.length; i ++) {
-
-var paragraph = document.createElement("p"); 
-paragraph.appendChild(document.createTextNode(gedicht.strophen[i]));
-container.appendChild(paragraph).classList.add("anotherclass_"+i);
-
-}
-
-// click scale paragraphs 
-
-for(var i = 0; i < gedicht.strophen.length; i ++) {
-
-const clickp = document.querySelector(".anotherclass_"+i);
-clickp.addEventListener("click", function(){;
-	clickp.classList.toggle("zoom")
-
-}, false);
-
-}
-
-// Animation
-
-var start = null;
-var elements = document.querySelector('.gedicht');
-elements.style.position = "absolute";
-
-function step(timestamp) {
-  if (!start) start = timestamp;
-  var progress = timestamp - start;
-  elements.style.left = 0 + 'px';
-  if (progress < 2000) {
-    window.requestAnimationFrame(step);
-  }
-}
-
-elements.addEventListener("click", function() { 
-	window.requestAnimationFrame(step);
-
-	panther.style.transform= "scale(2)";
-
-}, false);
+outerlayer.addEventListener('mouseover', function() {
+  innerlayer.classList.add("move");
+}, false)
 
 
-// Task : add a new class when transition is over. Then back again.
+outerlayer.addEventListener('mouseout', function() {
+  innerlayer.classList.remove("move");
+}, false)
+
+
+
+
+
+
+
 
